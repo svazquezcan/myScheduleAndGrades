dec3a906aa76992fc580c05588c40be227d67e90
Oriol Gonzalo <ogonzalo@uoc.edu>
2021-05-23 16:38:01 +0200

    Corregido fallo menor en expediente

--- app/Models/Percentage.php
+++ app/Models/Percentage.php
@@ -17,4 +17,18 @@ class Percentage {
         $percentages = DB::table('percentage')->where('id_class', $subjectId)->first();
         return json_decode(json_encode($percentages), true);
     }
+
+    /**
+     * Editar los porcentajes de evaluación para una asignatura del curso.
+     */
+    public function edit($percentage)
+    {
+        if($percentage['continuous_assessment'] + $percentage['exams']  <= 100) {
+            $percentage = DB::table('percentage')->where('id_percentage',$percentage['id_percentage'])->update([
+                'continuous_assessment'=>$percentage['continuous_assessment'],
+                'exams'=>$percentage['exams'],
+            ]); 
+        }
+        
+    }
 }
